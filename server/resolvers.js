e696f2e32d135c95bcde6ed21aa4a5c31dc9562a
Adithya Namada <AdithyaN@emids.com>
2023-04-19 15:03:39 +0530

    integrated spring boot api's into the application for mock data

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,30 +1,63 @@
-const { ProviderList, patientList } = require("../mockdata");
-const { PrismaClient } = require("@prisma/client");
+//const { ProviderList, patientList } = require("../mockdata");
+const asyncHandler = require("express-async-handler");
+const axios = require('axios');
 const _ = require("lodash");
 
 const resolvers = {
   Query: {
     providers: () => {
-        return ProviderList;
+        return getProviderList();
       }, 
       exactPatientName:(parent, args, context, info) =>{
       const firstName = args.firstName;
-      
-      return _.find(patientList, { firstName });
+      return new Promise((resolve,rejecj)=>{
+        getPatientList().then((result) =>{
+        const res = _.find(result, { firstName });
+        resolve(res);
+        });
+      })
       },
       patients:(parent, args) =>{
         
-        const firstName = args.firstName;
-        const patient = patientList.filter(o => o.firstName.includes(firstName));
-        return patient;
+          const firstName = args.firstName;
+          return new Promise((resolve,rejecj)=>{
+          getPatientList().then((result) =>{
+          const res = result.filter(o => o.firstName.includes(firstName));
+          resolve(res);
+          });
+        })       
       }
   },
   Provider: {
     patients: () => {
-      return patientList
+     return getPatientList();
     },
   }
  
 };
+
+const getProviderList = asyncHandler(async (req, res) => {
+  try{
+
+    let resp = await axios.get('http://localhost:8083/graphql/getProviderList');
+    return resp.data;
+  }
+  catch(err) {
+    console.log("Error mesg",err);
+  }
+ });
+
+ const getPatientList = asyncHandler(async (req, res) => {
+  try{
+    let res=[];
+    await axios.get('http://localhost:8083/graphql/getPatientList').then(finalres=>{
+    res = finalres.data;
+  });
+  return res;
+  }
+  catch(err) {
+    console.log("Error mesg",err);
+  }
+ });
 
 module.exports = { resolvers };
